4d7a577584069f9fc6f889acc876c1013a5ecdae
vaibhav <ankitkumar35546567@gmail.com>
2023-07-17 09:36:18 +0530

    Added mongoose to use mongodb

--- app.js
+++ app.js
@@ -4,24 +4,51 @@ const port = 3000;
 
 const app = express();
 
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [];
-let workItems = [];
+
+// for db connection
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/todoListDB', { useNewUrlParser: true });
+
+// creating db schema 
+const itemSchema = mongoose.Schema({
+    name: String
+});
+
+// creating model from schema
+const Item = mongoose.model('Item', itemSchema);
+
+const item1 = new Item({
+    name: 'Welcome to ToDo List'
+});
+
+const item2 = new Item({
+    name: 'Press + button to add new items'
+});
+
+const item3 = new Item({
+    name: 'Press - button to delete an entry'
+});
+
+const defaultItem = [item1, item2, item3];
 
 app.get("/", (req, res) => {
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    }
 
-    let day = today.toLocaleDateString("en-US", options);
-
-    res.render('index', { listTitle: day, newItem: items });
+    Item.find({}).then(items => {
+        const foundItem = items;
+        // console.log(foundItem.length);
+        if (foundItem.length == 0) {
+            Item.insertMany(defaultItem);
+            res.redirect("/");
+        } else {
+            res.render('index', { listTitle: "Today", newItem: foundItem });
+        }
+    }).catch(err => console.log('Caught', err.message));
 });
 
 
